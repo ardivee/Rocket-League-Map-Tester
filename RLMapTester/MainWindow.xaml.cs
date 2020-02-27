76cac4051e6b5f7f3875b3fec0b0fe4e14c1d025
Ardivee <rik1992devries@gmail.com>
2020-02-27 10:28:31 +0100

    Add project files.

---
+++ RLMapTester/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RLMapTester
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string mapPath = Properties.Settings.Default.MapFolder;
+        public string modPath = Properties.Settings.Default.ModsFolder;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            UpdateMapList();
+        }
+
+        private void UpdateMapList()
+        {
+            if (!System.IO.Directory.Exists(mapPath))
+                return;
+
+            MapList.Items.Clear();
+
+            var maps = System.IO.Directory.EnumerateFiles(mapPath, "*.*", System.IO.SearchOption.TopDirectoryOnly).Where(s => s.EndsWith(".udk") || s.EndsWith(".upk"));
+
+            foreach (string map in maps)
+            {
+                var mapname = System.IO.Path.GetFileNameWithoutExtension(map);
+                var lvi = new ListViewItem();
+                lvi.Content = mapname;
+                lvi.Tag = map;
+                MapList.Items.Add(lvi);
+            }
+        }
+
+        private void RefreshBtn_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateMapList();
+        }
+
+        private void UpdateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (!System.IO.Directory.Exists(modPath))
+            {
+                MessageBox.Show("Mod folder not set!");
+                return;
+            }
+
+            var selectedMap = (ListViewItem)MapList.SelectedItem;
+
+            if(selectedMap != null)
+            {
+                var map = (string)selectedMap.Tag;
+
+                System.IO.File.Copy(map, System.IO.Path.Combine(modPath, "Labs_Underpass_P.upk"), true);
+
+                MessageBox.Show("Copied!");
+            } else
+            {
+                MessageBox.Show("No map selected!");
+            }
+        }
+
+        private void SettingsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new SettingWindow(this);
+            window.Show();
+        }
+    }
+}
---
+++ RLMapTester/SettingWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RLMapTester
+{
+    /// <summary>
+    /// Interaction logic for SettingWindow.xaml
+    /// </summary>
+    public partial class SettingWindow : Window
+    {
+        MainWindow mainWindow;
+
+        public SettingWindow(MainWindow window)
+        {
+            InitializeComponent();
+
+            MapsFolder.Text = Properties.Settings.Default.MapFolder;
+            ModsFolder.Text = Properties.Settings.Default.ModsFolder;
+
+            mainWindow = window;
+        }
+
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.MapFolder = MapsFolder.Text;
+            Properties.Settings.Default.ModsFolder = ModsFolder.Text;
+            Properties.Settings.Default.Save();
+
+            mainWindow.mapPath = MapsFolder.Text;
+            mainWindow.modPath = ModsFolder.Text;
+
+            MessageBox.Show("Saved");
+        }
+
+        private void SelectMapFolderBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new FolderSelectDialog
+            {
+                Title = "Select your Maps Folder"
+            };
+
+            if (dialog.Show())
+            {
+                if (!string.IsNullOrWhiteSpace(dialog.FileName))
+                {
+                    MapsFolder.Text = dialog.FileName;
+                }
+            }
+        }
+
+        private void SelectModFolderBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new FolderSelectDialog
+            {
+                Title = "Select your Mods Folder"
+            };
+
+            if (dialog.Show())
+            {
+                if (!string.IsNullOrWhiteSpace(dialog.FileName))
+                {
+                    ModsFolder.Text = dialog.FileName;
+                }
+            }
+        }
+    }
+}
